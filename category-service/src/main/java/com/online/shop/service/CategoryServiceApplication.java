e3d70fee0d47fd2ba4d56124a6bd9dc4de0ba667
ismalsagir <sagir.66@hotmail.com>
2023-01-25 18:49:30 +0100

    online shop

---
+++ category-service/src/main/java/com/online/shop/service/CategoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.online.shop.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryServiceApplication.class,args);
+    }
+}
